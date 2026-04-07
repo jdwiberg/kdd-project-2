4e60a4e6a53d93823790c8664137da2b51416888
jdwiberg <jacobwiberg1133@gmail.com>
2026-04-07 13:24:10 -0400

    stubs

---
+++ classification.py
@@ -0,0 +1,14 @@
+from preprocessing import classification_pp # returns attributes and target (X, y) for regression dataset
+
+
+
+def classification():
+    X, y = classification_pp()
+    return
+    
+
+def main():
+    classification()
+
+if __name__ == "__main__":
+    classification()
---
+++ regression.py
@@ -0,0 +1,13 @@
+from preprocessing import regression_pp # returns attributes and target (X, y) for regression dataset
+
+
+def regression():
+    X, y = regression_pp()
+    return
+    
+
+def main():
+    regression()
+    
+if __name__ == "__main__":
+    regression()
